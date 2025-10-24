cf7ddfe2b291ef781c694f616324547a57af3980
brondonnono <brondonnono3@gmail.com>
2025-10-24 19:51:02 +0100

    starting to update user Service

--- src/app/services/member.service.ts
+++ src/app/services/member.service.ts
@@ -1,50 +1,83 @@
 import { Injectable, inject } from '@angular/core';
 import { ElectronService } from './electron.service';
 import { Member } from '../core/models/Member';
+import { Store } from '@ngrx/store';
+import { User } from '../core/models/User';
+import { selectUser } from '../core/state/auth/auth.selector';
+import { LogService } from './log.service';
+import { ActionType, TargetType } from '../core/enums/ActionType.enum';
 
 @Injectable({ providedIn: 'root' })
 export class MemberService {
   private electron = inject(ElectronService);
+  private logService = inject(LogService);
+  private store = inject(Store);
+  private currentUser: User | null | undefined = null;
+
+  constructor() {
+    this.store.select(selectUser).subscribe((res) => (this.currentUser = res));
+  }
 
   async getAll(): Promise<Member[]> {
     return await this.electron.runQuery('SELECT * FROM members');
   }
 
   async add(member: Omit<Member, 'id' | 'created_at' | 'updated_at'>) {
-    const id = crypto.randomUUID();
-    const created_at = new Date().toISOString();
-    await this.electron.runQuery(
-      `INSERT INTO members (id,name,phone,cni,status,joined_date,created_at,updated_at) VALUES (?,?,?,?,?,?,?)`,
-      [
-        id,
-        member.name,
-        member.phone,
-        member.cni,
-        member.status,
-        member.joined_date,
-        created_at,
-        created_at,
-      ]
-    );
+    if (this.currentUser) {
+      const id = crypto.randomUUID();
+      const created_at = new Date().toISOString();
+      await this.electron.runQuery(
+        `INSERT INTO members (id,name,phone,cni,status,joined_date,created_at,updated_at) VALUES (?,?,?,?,?,?,?)`,
+        [
+          id,
+          member.name,
+          member.phone,
+          member.cni,
+          member.status,
+          member.joined_date,
+          created_at,
+          created_at,
+        ]
+      );
+      await this.logService.add(
+        ActionType.CREATE.toUpperCase() + '_' + TargetType.MEMBER.toUpperCase(),
+        this.currentUser.id,
+        id
+      );
+    }
   }
 
   async update(member: Member) {
-    const updated_at = new Date().toISOString();
-    await this.electron.runQuery(
-      `UPDATE members SET name=?, phone=?, cni=?, status=?, joined_date=?, updated_at=? WHERE id=?`,
-      [
-        member.name,
-        member.phone,
-        member.cni,
-        member.status,
-        member.joined_date,
-        updated_at,
-        member.id,
-      ]
-    );
+    if (this.currentUser) {
+      const updated_at = new Date().toISOString();
+      await this.electron.runQuery(
+        `UPDATE members SET name=?, phone=?, cni=?, status=?, joined_date=?, updated_at=? WHERE id=?`,
+        [
+          member.name,
+          member.phone,
+          member.cni,
+          member.status,
+          member.joined_date,
+          updated_at,
+          member.id,
+        ]
+      );
+      await this.logService.add(
+        ActionType.EDIT.toUpperCase() + '_' + TargetType.MEMBER.toUpperCase(),
+        this.currentUser.id,
+        member.id
+      );
+    }
   }
 
   async delete(id: string) {
-    await this.electron.runQuery(`DELETE FROM members WHERE id=?`, [id]);
+    if (this.currentUser) {
+      await this.electron.runQuery(`DELETE FROM members WHERE id=?`, [id]);
+      await this.logService.add(
+        ActionType.DELETE.toUpperCase() + '_' + TargetType.MEMBER.toUpperCase(),
+        this.currentUser.id,
+        id
+      );
+    }
   }
 }
--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,10 +1,68 @@
-import { Injectable } from '@angular/core';
+import { inject, Injectable } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { ActionType, TargetType } from '../core/enums/ActionType.enum';
+import { Member } from '../core/models/Member';
+import { User } from '../core/models/User';
+import { selectUser } from '../core/state/auth/auth.selector';
+import { ElectronService } from './electron.service';
+import { LogService } from './log.service';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class UserService {
+  private electron = inject(ElectronService);
+  private logService = inject(LogService);
+  private store = inject(Store);
+  private currentUser: User | null | undefined = null;
 
-  constructor() { }
+  constructor() {
+    this.store.select(selectUser).subscribe((res) => (this.currentUser = res));
+  }
 
+  async getAll(): Promise<Member[]> {
+    return await this.electron.runQuery('SELECT * FROM users');
+  }
+
+  async add(user: Omit<User, 'id' | 'created_at'>) {
+    if (this.currentUser) {
+      const id = crypto.randomUUID();
+      const created_at = new Date().toISOString();
+      await this.electron.runQuery(
+        `INSERT INTO users (id,username,password,created_at) VALUES (?,?,?,?)`,
+        [id, user.username, user.password, created_at]
+      );
+      await this.logService.add(
+        ActionType.CREATE.toUpperCase() + '_' + TargetType.USER.toUpperCase(),
+        this.currentUser.id,
+        id
+      );
+    }
+  }
+
+  async update(user: User) {
+    if (this.currentUser) {
+      await this.electron.runQuery(`UPDATE users SET username=?, password=? WHERE id=?`, [
+        user.username,
+        user.password,
+        user.id,
+      ]);
+      await this.logService.add(
+        ActionType.EDIT.toUpperCase() + '_' + TargetType.USER.toUpperCase(),
+        this.currentUser.id,
+        user.id
+      );
+    }
+  }
+
+  async delete(id: string) {
+    if (this.currentUser) {
+      await this.electron.runQuery(`DELETE FROM users WHERE id=?`, [id]);
+      await this.logService.add(
+        ActionType.DELETE.toUpperCase() + '_' + TargetType.USER.toUpperCase(),
+        this.currentUser.id,
+        id
+      );
+    }
+  }
 }
