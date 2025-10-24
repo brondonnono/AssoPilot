1a5da441d2f296b0fde23a2d6a605ea6d272bdde
brondonnono <brondonnono3@gmail.com>
2025-10-24 06:59:46 +0100

    Update of cotisation service

--- src/app/services/cotisation.service.ts
+++ src/app/services/cotisation.service.ts
@@ -1,49 +1,107 @@
 import { Injectable, inject } from '@angular/core';
 import { ElectronService } from './electron.service';
 import { Cotisation } from '../core/models/Cotisation';
+import { Store } from '@ngrx/store';
+import { User } from '../core/models/User';
+import { LogService } from './log.service';
+import { selectUser } from '../core/state/auth/auth.selector';
+import { ActionType, TargetType } from '../core/enums/ActionType.enum';
 
 @Injectable({ providedIn: 'root' })
 export class CotisationService {
   private electron = inject(ElectronService);
+  private logService = inject(LogService);
+  private store = inject(Store);
+  private currentUser: User | null | undefined = null;
+
+  constructor() {
+    this.store.select(selectUser).subscribe((res) => (this.currentUser = res));
+  }
 
   async getAll(): Promise<Cotisation[]> {
     return await this.electron.runQuery('SELECT * FROM cotisations');
   }
 
   async add(c: Omit<Cotisation, 'id' | 'created_at' | 'updated_at'>) {
-    const id = crypto.randomUUID();
-    const created_at = new Date().toISOString();
-    await this.electron.runQuery(
-      `INSERT INTO cotisations (id,title,amount,frequency,start_date,created_at) VALUES (?,?,?,?,?,?)`,
-      [id, c.title, c.amount, c.frequency, c.start_date, created_at]
-    );
-    return id;
+    if (this.currentUser) {
+      const id = crypto.randomUUID();
+      const created_at = new Date().toISOString();
+      await this.electron.runQuery(
+        `INSERT INTO cotisations (id,title,amount,frequency,start_date,created_at) VALUES (?,?,?,?,?,?)`,
+        [id, c.title, c.amount, c.frequency, c.start_date, created_at]
+      );
+
+      await this.logService.add(
+        ActionType.CREATE.toUpperCase() + '_' + TargetType.COTISATION.toUpperCase(),
+        this.currentUser.id,
+        id
+      );
+      return id;
+    }
+    return;
   }
 
   async update(c: Cotisation) {
-    const updated_at = new Date().toISOString();
-    await this.electron.runQuery(
-      `UPDATE cotisations SET title=?, amount=?, frequency=?, start_date=?, updated_at=? WHERE id=?`,
-      [c.title, c.amount, c.frequency, c.start_date, updated_at, c.id]
-    );
+    if (this.currentUser) {
+      const updated_at = new Date().toISOString();
+      await this.electron.runQuery(
+        `UPDATE cotisations SET title=?, amount=?, frequency=?, start_date=?, updated_at=? WHERE id=?`,
+        [c.title, c.amount, c.frequency, c.start_date, updated_at, c.id]
+      );
+
+      await this.logService.add(
+        ActionType.EDIT.toUpperCase() + '_' + TargetType.COTISATION.toUpperCase(),
+        this.currentUser.id,
+        c.id
+      );
+    }
   }
 
   async delete(id: string) {
-    await this.electron.runQuery(`DELETE FROM cotisations WHERE id=?`, [id]);
+    if (this.currentUser) {
+      await this.electron.runQuery(`DELETE FROM cotisations WHERE id=?`, [id]);
+      await this.logService.add(
+        ActionType.DELETE.toUpperCase() + '_' + TargetType.COTISATION.toUpperCase(),
+        this.currentUser.id,
+        id
+      );
+    }
   }
 
   async attachMember(cotisationId: string, memberId: string) {
-    await this.electron.runQuery(
-      `INSERT OR IGNORE INTO cotisation_members (cotisation_id, member_id) VALUES (?, ?)`,
-      [cotisationId, memberId]
-    );
+    if (this.currentUser) {
+      await this.electron.runQuery(
+        `INSERT OR IGNORE INTO cotisation_members (cotisation_id, member_id) VALUES (?, ?)`,
+        [cotisationId, memberId]
+      );
+      await this.logService.add(
+        ActionType.ATTACH.toUpperCase() +
+          '_' +
+          TargetType.MEMBER.toUpperCase() +
+          '_TO_' +
+          TargetType.COTISATION.toUpperCase(),
+        this.currentUser.id,
+        memberId
+      );
+    }
   }
 
   async detachMember(cotisationId: string, memberId: string) {
-    await this.electron.runQuery(
-      `DELETE FROM cotisation_members WHERE cotisation_id=? AND member_id=?`,
-      [cotisationId, memberId]
-    );
+    if (this.currentUser) {
+      await this.electron.runQuery(
+        `DELETE FROM cotisation_members WHERE cotisation_id=? AND member_id=?`,
+        [cotisationId, memberId]
+      );
+      await this.logService.add(
+        ActionType.DETACH.toUpperCase() +
+          '_' +
+          TargetType.MEMBER.toUpperCase() +
+          '_TO_' +
+          TargetType.COTISATION.toUpperCase(),
+        this.currentUser.id,
+        memberId
+      );
+    }
   }
 
   async getMembers(cotisationId: string) {
